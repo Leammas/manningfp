35667bea97b2894a51de25939580c1d83b1a5865
Vladimir Samoylov <leammas@gmail.com>
2017-02-01 12:07:20 +0300

    FSM

--- src/main/scala/State.scala
+++ src/main/scala/State.scala
@@ -1,4 +1,91 @@
-object State extends App {
+import FSM._
+import State._
+
+import scala.annotation.tailrec
+import scala.collection.immutable.::
+
+case class State[S, +A](run: S => (A, S)) {
+  def map[B](f: A => B): State[S, B] = {
+    flatMap(x => unit(f(x)))
+  }
+
+  def flatMap[B](g: A => State[S, B]): State[S, B] = State {
+    s => {
+      val (a, s2) = this.run(s)
+      g(a).run(s2)
+    }
+  }
+
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] = {
+    for {
+      a <- this
+      b <- sb
+      s <- unit(f(a, b))
+    } yield s
+  }
+
+  def both[B](sb: State[S, B]): State[S, (A, B)] = map2(sb)(_ -> _)
+}
+
+object State {
+  def unit[S, A](a: A): State[S, A] = State(s => a -> s)
+
+  def sequence[S, A](fs: List[State[S, A]]): State[S, List[A]] = {
+    @tailrec
+    def loop(z: List[State[S, A]])(a: State[S, List[A]]): State[S, List[A]] = {
+      z match {
+        case Nil => a
+        case x :: Nil =>
+          x.map2(a)(_ :: _)
+        case x1 :: x2 :: xs =>
+          val l = x1.map2(x2)(List(_, _))
+          val newl = l.map2(a)(_ ++ _)
+          loop(xs)(newl)
+      }
+    }
+
+    loop(fs)(unit(List.empty[A]))
+  }
+}
+
+object FSM {
+
+  sealed trait Input
+
+  case object Coin extends Input
+
+  case object Turn extends Input
+
+  case class Machine(locked: Boolean, candies: Int, coins: Int) {
+    def next(input: Input): Machine = {
+      input match {
+        case Coin =>
+          if (locked) {
+            copy(locked = false, coins = coins + 1)
+          } else {
+            copy(coins = coins + 1)
+          }
+        case Turn =>
+          if (!locked) {
+            copy(locked = true, candies = candies - 1)
+          } else {
+            this
+          }
+      }
+    }
+  }
+
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
+    val folded = (m: Machine) => {
+      inputs.foldLeft(m)((z, a) => z.next(a))
+    }
+    State(folded.andThen(newm => (newm.coins, newm.candies) -> newm))
+  }
+
+
+}
+
+object StateEx extends App {
 
   case class SimpleRNG(seed: Long) extends RNG {
     def nextInt: (Int, RNG) = {
@@ -13,61 +100,31 @@ object State extends App {
     def nextInt: (Int, RNG)
   }
 
-  type Rand[+A] = (RNG) => (A, RNG)
+  type Rand[A] = State[RNG, A]
 
   def ints(c: Int): Rand[List[Int]] = {
     sequence(List.fill(c)(int))
   }
 
-  val int: Rand[Int] = _.nextInt
-
-  def map[A, B](s: Rand[A])(f: A => B): Rand[B] = {
-    rng => {
-      val (a, s2) = s(rng)
-      (f(a), s2)
-    }
-  }
+  val int: Rand[Int] = State(_.nextInt)
 
   val nnEven: Rand[Int] =
-    map(int)(i => i - i % 2)
-
-  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
-    rng => {
-      val (a, r2) = ra(rng)
-      val (b, r3) = rb(r2)
-      f(a, b) -> r3
-    }
-  }
-
-  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] = map2(ra, rb)(_ -> _)
+    int.map(i => i - i % 2)
 
   val double: Rand[Double] = {
-    map(int)(_.toDouble / Int.MaxValue)
+    int.map(_.toDouble / Int.MaxValue)
   }
 
-  def pure[A](a: A): Rand[A] = rng => a -> rng
+  assert({
+    val a = SimpleRNG(1L)
+    ints(5).run(a) == (List(384748, -1151252339, -549383847, 1612966641, -883454042),SimpleRNG(223576932655868L))
+  }, "RNG is not reproducable")
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
-    def loop(z: List[Rand[A]])(a: Rand[List[A]]): Rand[List[A]] = {
-      z match {
-        case Nil => a
-        case x :: Nil =>
-          map2(x, a)(_ :: _)
-        case x1 :: x2 :: xs =>
-          val l = map2(x1, x2)(List(_, _))
-          val newl = map2(l, a)(_ ++ _)
-          loop(xs)(newl)
-      }
-    }
-
-    loop(fs)(pure(List.empty[A]))
-  }
-
-  val rid = both(int, double)
-
-  val a = SimpleRNG(1L)
-
-  println(ints(5)(a))
-
+  assert({
+    val m = Machine(true, 5, 10)
+    val cmds: List[Input] = List(Coin, Turn, Coin, Turn, Coin, Turn, Coin, Turn)
+    val ((coins, candies), m2) = simulateMachine(cmds).run(m)
+    coins == 14 && candies == 1
+  }, "FSM failed!")
 
 }
